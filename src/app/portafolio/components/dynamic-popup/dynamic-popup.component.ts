59094f62d35023f5c3227552adbc61ff15db859d
Kas <nicolasgomez.dev@gmail.com>
2023-04-27 22:00:20 -0500

    Se solucionan problemas de MOdal Dinamico

--- src/app/portafolio/components/dynamic-popup/dynamic-popup.component.ts
+++ src/app/portafolio/components/dynamic-popup/dynamic-popup.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { PortafolioCard } from '../../interfaces/portafolioCard.interface';
 
 @Component({
   selector: 'app-dynamic-popup',
@@ -8,7 +9,27 @@ import { Component, Input } from '@angular/core';
 })
 export class DynamicPopupComponent {
 
-  @Input() id = ""; 
+  // @Input() id = "";
+  // @Input() title = "";
+  // @Input() subTitle="";
+  // @Input() objetivo="";
+  // @Input() metodologia="";
+  // @Input() tecnologias= [];
+
+  @Input() card:PortafolioCard ={
+      id : '',
+      imgDestacada: '',
+      summary:'',
+      date:'',
+      title : '',
+      subTitle:'',
+      objetivo:'',
+      metodologia:[],
+      tecnologias:[],
+      poster:'',
+      enlace: ''
+    };
+
 
 
 }
--- src/app/portafolio/interfaces/portafolioCard.interface.ts
+++ src/app/portafolio/interfaces/portafolioCard.interface.ts
@@ -1,6 +1,22 @@
 export interface PortafolioCard{
 
-  id:string
+  id:string,
+  imgDestacada:string,
+  title:string,
+  subTitle:string,
+  summary:string,
+  date:string,
+  objetivo:string,
+  metodologia:string[],
+  tecnologias:Tecnologias[],
+  poster:string,
+  enlace?:string
 
 
+}
+
+
+export interface Tecnologias{
+  imagen:string,
+  texto:string
 }
